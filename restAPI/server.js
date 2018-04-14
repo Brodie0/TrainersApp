553d02476549521ac04fc6a2fc57ff9021e3c918
Mateusz <mateuszw.gt@gmail.com>
2018-04-14 14:57:36 +0200

    added create,read and update of users

--- restAPI/server.js
+++ restAPI/server.js
@@ -4,14 +4,15 @@ const app = express()
 
 var mysql = require('mysql')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
 var connection = mysql.createConnection({
     host: '192.168.0.94',
     user: "root",
     password: "kramptopedal",
     database: "db"
 })
-
-
 
 app.listen(3000, function() {
     console.log('3000')
@@ -30,11 +31,45 @@ app.get('/test', function(){
     })
 })
 
-app.get('/dej', function(){
+app.get('/users', function(req, res, next){
     connection.query('SELECT * FROM users', (err, result, fields) => {
         if (err) throw err
+        
+        res.json({result})
+    })
+})
 
-        console.log('result: ')
-        console.log(result[0].login)
+app.get('/users/:id', (req, res, next) => {
+    connection.query('SELECT * FROM users WHERE user_id=' + req.params.id, (err, result, fields) => {
+        if(err) throw err
+        console.log(req.params.id)
+        res.json(result[0])
+    })
+})
+
+app.post('/newUser', (req, res, next) => {
+    connection.query(`SELECT * FROM users WHERE login="${req.body.login}"`, (err, result, fields) => {
+        
+        if(result == undefined){
+            
+            connection.query(`INSERT INTO users(login, password) VALUES ("${req.body.login}","'${req.body.pass}");`, (err, result) => {
+                if (err) throw err
+                
+                console.log('user added')
+                res.json({'message' : 'user added'})
+            })   
+        
+        } else {
+            console.log('user login exists')
+            res.json({'message' : 'user login exists'})
+        }
+    })  
+})
+
+app.put('/updateUser', (req, res, next) => {
+    connection.query(`UPDATE users SET password="${req.body.pass}" WHERE login="${req.body.login}";`, (err, result) => {
+        if (err) throw err
+
+        console.log('user updated')
     })
 })
