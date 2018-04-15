32320c3c34ed597ef2eddc680ed6957f4e836a70
giteq <patrykkotlowski@wp.pl>
2018-04-15 03:21:55 +0200

    Added working GPS

--- app/src/main/java/program/trainersapp/Info.kt
+++ app/src/main/java/program/trainersapp/Info.kt
@@ -1,8 +1,10 @@
 package program.trainersapp
 
 import android.app.Activity
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_info.*
 import program.trainersapp.model.Dto
 
@@ -12,7 +14,7 @@ class Info : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
-        val Database = Dto()
+        val database = Dto()
 
         //On Click Actions
         this.stats.setOnClickListener {
@@ -24,7 +26,7 @@ class Info : Activity() {
             this.history.setBackgroundColor(Color.CYAN)
             this.stats.setBackgroundColor(Color.WHITE)
             this.friends.setBackgroundColor(Color.WHITE)
-            val jsonHistory = Database.getById("records", 1)
+            val jsonHistory = database.getById("records", 1)
             jsonHistory.length
         }
         this.friends.setOnClickListener {
@@ -32,6 +34,10 @@ class Info : Activity() {
             this.history.setBackgroundColor(Color.WHITE)
             this.stats.setBackgroundColor(Color.WHITE)
         }
+    }
 
+    fun switchToMapActivity(view : View){
+        val next = Intent(this, GPS::class.java)
+        startActivity(next)
     }
 }
